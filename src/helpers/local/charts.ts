ff99e337f39a316242b3ee700e2767153d9025a7
A1xUpH0 <davidallais2001@gmail.com>
2023-08-08 12:57:11 +0200

    WIP

--- src/helpers/local/charts.ts
+++ src/helpers/local/charts.ts
@@ -1,1 +1,19 @@
-import plotly from 'plotly.js';
+import fs from "fs";
+
+import sharp from "sharp";
+import vega, { Spec } from "vega";
+
+import LineChart from "./charts/line-chart.json";
+
+
+const view = new vega.View(vega.parse(LineChart as Spec), { renderer: "none" });
+
+view.toSVG().then(async function (svg) {
+
+    await sharp(Buffer.from(svg))
+        .toFormat("png")
+        .toFile("fileName.png");
+
+}).catch(function (err) {
+    console.error(err);
+});
