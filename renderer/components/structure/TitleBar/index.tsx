ece4a6074789b8f0bd7c01556441a05cd177d952
yoratoni <71235233+yoratoni@users.noreply.github.com>
2023-12-21 09:29:35 +0100

    WIP

--- renderer/components/structure/TitleBar/index.tsx
+++ renderer/components/structure/TitleBar/index.tsx
@@ -6,7 +6,7 @@ import TitleBarButton from "@/components/base/Button/TitleBarButton";
 export default function TitleBar() {
     return (
         <div className="w-full bg-red-500 flex items-center justify-between pl-2">
-            <h1>fesse</h1>
+            <h1>TEST</h1>
 
             <div className="flex p-0 m-0">
                 <TitleBarButton
