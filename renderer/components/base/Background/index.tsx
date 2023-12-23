fecab1521cac3eea5c59e56086a53e4f102cd3dd
yoratoni <71235233+yoratoni@users.noreply.github.com>
2023-12-23 23:21:09 +0100

    WIP

--- renderer/components/base/Background/index.tsx
+++ renderer/components/base/Background/index.tsx
@@ -3,24 +3,14 @@ import Image from "next/image";
 
 export default function Background() {
     return (
-        <div className="absolute -z-10 inset-0 bg-primary-1200 w-full h-full flex items-center justify-center">
-            <div className="absolute min-w-[200vw] aspect-square">
+        <div className="absolute -z-10 inset-0 bg-black w-full h-full flex items-center justify-center">
+            <div className="absolute min-w-[200vh] aspect-square">
                 <Image
                     src="/static/images/background/cybearl.webp"
                     alt="Cybearl background"
                     fill
                     objectFit="cover"
-                    className="opacity-30 overflow-visible blur-sm"
-                />
-            </div>
-
-            <div className="relative w-full h-full">
-                <Image
-                    src="/static/images/background/cybearl.webp"
-                    alt="Cybearl background"
-                    fill
-                    objectFit="contain"
-                    className="opacity-50 drop-shadow-2xl"
+                    className="opacity-20 overflow-visible"
                 />
             </div>
         </div>
--- renderer/components/structure/Layout/index.tsx
+++ renderer/components/structure/Layout/index.tsx
@@ -19,23 +19,22 @@ export default function Layout({
         <div className={`${Inconsolata.className} relative w-full h-screen min-h-screen overflow-hidden flex flex-col`}>
             <Background />
 
+            <header className="w-full z-0">
+                <Nav />
+            </header>
+
             {appStatus === "loading" && (
                 <div
                     className="absolute z-20 inset-0 w-full h-full items-center flex justify-center"
                     onClick={(e) => e.preventDefault()}
                 >
-                    <Background />
-                    <h4 className="text-3xl">LOADING...</h4>
+
                 </div>
             )}
 
             <main className="w-full flex-grow z-0 p-4 flex items-start justify-start flex-col">
                 {children}
             </main>
-
-            <footer className="w-full z-0">
-                <Nav />
-            </footer>
         </div>
     );
 }
