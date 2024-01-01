02881dac15ff6ef1f69fdef8d3c2df3e4699c8d5
yoratoni <71235233+yoratoni@users.noreply.github.com>
2024-01-01 11:09:50 +0100

    WIP

--- main/api/index.ts
+++ main/api/index.ts
@@ -1,6 +1,10 @@
 import { ipcMain } from "electron";
 
 
+/**
+ * The main api entry point used for the IPC communication
+ * between the main and the renderer process.
+ */
 export default function ipcHandler() {
     ipcMain.on("ping", async (_event, value) => {
         console.log(value);
